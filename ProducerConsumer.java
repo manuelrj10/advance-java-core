e9260e3d0b11ff1175c0fcb279329609b368dae5
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-03-31 15:45:53 +0530

    Add files via upload

---
+++ ProducerConsumer.java
@@ -0,0 +1,26 @@
+package threadex;
+
+
+class Store{
+	synchronized void produce() {
+		
+	}
+	synchronized void consumes() {
+		
+	}
+}
+class conumer extends Thread{
+	
+}
+class producer extends Thread{
+	
+}
+
+public class ProducerConsumer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
